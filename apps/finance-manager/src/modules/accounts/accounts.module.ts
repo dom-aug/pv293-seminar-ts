bef411f23cdbc01c33ae10b384c14770fc9e88c2
Dominik Augustin <87132867+dom-aug@users.noreply.github.com>
2025-11-02 15:36:21 +0100

    refactor: reorganize accounts module imports and providers to enhance structure

--- apps/finance-manager/src/modules/accounts/accounts.module.ts
+++ apps/finance-manager/src/modules/accounts/accounts.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AccountsController } from './accounts.controller';
-import { AccountsService } from './accounts.service';
+
+import { AccountsController } from './api/controllers/accounts.controller';
 import { AuthModule } from '../auth/auth.module';
-import { AccountsRepository } from './repositories/accounts.repository';
+import { AccountsRepository } from './infrastructure/repositories/accounts.repository';
+import { CreateAccountUseCase } from './application/create-account.use-case';
+import { FindAllAccountsUseCase } from './application/find-all-accounts.use-case';
+import { FindAccountByIdAndUserIdUseCase } from './application/find-account-by-id-and-userid.use-case';
+import { UpdateAccountUseCase } from './application/update-account.use-case';
+import { RemoveAccountUseCase } from './application/remove-account.use-case';
+import { GetAccountBalanceUseCase } from './application/get-account-balance.use-case';
+import { GetBalanceForAllUserAccountsUseCase } from './application/get-balance-for-all-user-accounts.use-case';
+
+const useCases = [
+  CreateAccountUseCase,
+  FindAllAccountsUseCase,
+  FindAccountByIdAndUserIdUseCase,
+  UpdateAccountUseCase,
+  RemoveAccountUseCase,
+  GetAccountBalanceUseCase,
+  GetBalanceForAllUserAccountsUseCase,
+];
 
 @Module({
   imports: [AuthModule],
   controllers: [AccountsController],
-  providers: [AccountsService, AccountsRepository],
-  exports: [AccountsService],
+  providers: [AccountsRepository, ...useCases],
+  exports: [AccountsRepository, CreateAccountUseCase],
 })
 export class AccountsModule {}
